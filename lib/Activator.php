6c6a68ded77a887dd8b3b9cbc2520753e9c9f280
Rui Sardinha <mail@ruisardinha.com>
2018-01-10 15:47:08 +0000

    adds Activator interface to handle plugin activation and deactivation

---
+++ lib/Activator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace csrui\WPConstruct\Plugin;
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      0.0.1
+ * @package    WPPlugin
+ * @author     Gulbenkian <devdigital@csrui.pt>
+ */
+interface Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    0.0.1
+	 */
+	public static function activate();
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    0.0.1
+	 */
+	public static function deactivate();
+}
